46312229f2b81bedf903471f7e6f023a5fce9db6
Richard Chen <richard.chen.dev@gmail.com>
2022-12-01 03:42:36 -0800

    Add thread argument passing and echo back to client

--- CSftp.c
+++ CSftp.c
@@ -21,6 +21,8 @@
 // #define PORT "3490"  // the port users will be connecting to
 
 #define BACKLOG 10   // how many pending connections queue will hold
+#define MAX_NUM_THREADS 10 // max number of child thread connections allowed
+#define BUFFER_SIZE 4096
 
 void sigchld_handler(int s)
 {
@@ -43,6 +45,46 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+// Multi-threading parameter passing from https://hpc-tutorials.llnl.gov/posix/example_code/hello_arg2.c
+struct thread_data {
+    int thread_id;
+    int new_fd;
+};
+
+struct thread_data thread_data_array[MAX_NUM_THREADS];
+
+void *command_handler(void *threadarg)
+{
+  int thread_id;
+  int new_fd;
+  struct thread_data *my_data;
+  my_data = (struct thread_data *) threadarg;
+  thread_id = my_data->thread_id;
+  new_fd = my_data->new_fd;
+
+  char buf[BUFFER_SIZE];
+  ssize_t read_size;
+  while((read_size = recv(new_fd , buf, BUFFER_SIZE - 1 , 0 )) > 0 ) {
+    buf[read_size] = '\0';
+
+    // test code to echo back to client
+    // implement parsing of the buffer into commands and arguments here
+    // put big switch statement
+    // https://canvas.ubc.ca/courses/101882/pages/tutorial-10-c-server-programming?module_item_id=5116919
+    printf("%s", buf);
+    char echo[BUFFER_SIZE + 6] = "echo: ";
+    strcat(echo, buf);
+    if (send(new_fd, echo, strlen(echo), 0) == -1)
+        perror("send");
+  }
+  
+
+  close(new_fd);
+  printf("%s\n", "finished child thread");
+  pthread_exit(NULL);
+}
+
+
 int main(int argc, char **argv)
 {
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
@@ -59,11 +101,14 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; // use my IP
 
+    // Checks if the program is run with anything other than an additional argument
+    // Displays a usage method if so
     if (argc != 2) {
       usage(argv[0]);
       return -1;
     }
 
+    // Gets address info using the port passed in, info then used to bind to socket later
     if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
@@ -114,8 +159,18 @@ int main(int argc, char **argv)
 
     printf("server: waiting for connections...\n");
 
+    pthread_t threads[MAX_NUM_THREADS];
+    int current_thread_id = 0;
+    int current_thread_count = 0;
+
     while(1) {  // main accept() loop
         sin_size = sizeof their_addr;
+
+        if (current_thread_count > MAX_NUM_THREADS) {
+            printf("server: exceeded max number of connections\n");
+            continue;
+        }
+
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (new_fd == -1) {
             perror("accept");
@@ -127,16 +182,21 @@ int main(int argc, char **argv)
             s, sizeof s);
         printf("server: got connection from %s\n", s);
 
-        if (!fork()) { // this is the child process
-            close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
-        }
+        thread_data_array[current_thread_id].thread_id = current_thread_id;
+        thread_data_array[current_thread_id].new_fd = new_fd;
+        current_thread_count++;
+        pthread_create(&threads[current_thread_id], NULL, command_handler, (void *) &thread_data_array[current_thread_id]);
+
+        pthread_join(threads[current_thread_id], NULL);
+        printf("%s\n", "end of main while loop");
         close(new_fd);  // parent doesn't need this
+        current_thread_id++;
+        current_thread_count--; 
+        
+        // exit(0); // call this from somewhere if we need to quit the server
     }
 
+    printf("%s\n", "out of main while loop end of main");
     return 0;
 }
 
